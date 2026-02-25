fe3c6370ef95671595ea160e8b428d6d406ebe9e
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-02-25 21:46:22 +0700

    first commit

---
+++ src/test/java/com/qiraht/spring_lms/SpringLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiraht.spring_lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
