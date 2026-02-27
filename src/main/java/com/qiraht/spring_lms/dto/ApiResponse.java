163ab3e981fc2b7717244a1fc5bf786de60cef48
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-02-27 18:26:37 +0700

    feat/ add ApiResponse DTO

---
+++ src/main/java/com/qiraht/spring_lms/dto/ApiResponse.java
@@ -0,0 +1,32 @@
+package com.qiraht.spring_lms.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    private final Integer status;
+    private final String message;
+    private final T data;
+
+    public static <T> ApiResponse<T> success(Integer status, String message, T data) {
+        return ApiResponse.<T>builder()
+                .status(status)
+                .message(message)
+                .data(data)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> error(Integer status, String message, T data) {
+        return ApiResponse.<T>builder()
+                .status(status)
+                .message(message)
+                .data(data)
+                .build();
+    }
+}
