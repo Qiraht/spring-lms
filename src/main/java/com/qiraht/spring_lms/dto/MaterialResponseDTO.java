4866cc83bc96d40457f5eefd29dc15ad66646fd0
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-01 00:34:24 +0700

    feat/ add Material Request and Response DTO

---
+++ src/main/java/com/qiraht/spring_lms/dto/MaterialResponseDTO.java
@@ -0,0 +1,10 @@
+package com.qiraht.spring_lms.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class MaterialResponseDTO {
+    String id;
+    String title;
+    String content;
+    String attachment;
+}
