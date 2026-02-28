a59ba31c8bf7481ac140d6c4722686fd6f5284a5
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-02-28 15:02:51 +0700

    feat/ add ClassResponseDTO to mask data

---
+++ src/main/java/com/qiraht/spring_lms/dto/ClassResponseDTO.java
@@ -0,0 +1,14 @@
+package com.qiraht.spring_lms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClassResponseDTO {
+    private String id;
+    private String name;
+    private String description;
+}
