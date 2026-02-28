bee461af640995de39c6ddbe2a344dc5988200a9
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-02-28 15:01:34 +0700

    feat/ add ClassesService getAllClasses, getClassById, updateClass, and DeleteClass

--- src/main/java/com/qiraht/spring_lms/service/ClassesService.java
+++ src/main/java/com/qiraht/spring_lms/service/ClassesService.java
@@ -1,6 +1,7 @@
 package com.qiraht.spring_lms.service;
 
 import com.qiraht.spring_lms.dto.ClassRequestDTO;
+import com.qiraht.spring_lms.dto.ClassResponseDTO;
 import com.qiraht.spring_lms.entity.Classes;
 import com.qiraht.spring_lms.repository.ClassesRepository;
 import com.soundicly.jnanoidenhanced.jnanoid.NanoIdUtils;
@@ -9,7 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -31,19 +35,41 @@ public class ClassesService {
         classesRepository.save(_class);
     }
 
-    public List<Classes> getAllClasses() {
-        return classesRepository.findAll();
+    public List<ClassResponseDTO> getAllClasses() {
+        List<ClassResponseDTO> response = new ArrayList<>();
+
+        for (Classes _class : classesRepository.findAll()) {
+            ClassResponseDTO responseDTO = new ClassResponseDTO();
+            BeanUtils.copyProperties(_class, responseDTO);
+            response.add(responseDTO);
+        }
+
+        return response;
     }
 
-    public Classes getClassById(String id) {
-        Classes classes = new Classes();
+    public ClassResponseDTO getClassById(String id) {
+        Classes _class = classesRepository.findById(id).orElseThrow(() -> new RuntimeException("Class with id " + id + " not found"));
 
-        classesRepository.findById(id).ifPresentOrElse(
-                c -> { BeanUtils.copyProperties(c, classes);},
-                () -> { throw new RuntimeException("Class with id " + id + " not found");}
-        );
+        ClassResponseDTO responseDTO = new ClassResponseDTO();
+        BeanUtils.copyProperties(_class, responseDTO);
+        return responseDTO;
+    }
 
-        return classes;
+    public void updateClass(String id,ClassRequestDTO request) {
+        log.info("Putting class: {}", request.getName());
+        Classes classes = classesRepository.findById(id).orElseThrow(() -> new RuntimeException("Class with id " + id + " not found"));
+
+        classes.setName(request.getName());
+        classes.setDescription(request.getDescription());
+
+        classesRepository.save(classes);
+    }
+
+    public void deleteClass(String id) {
+        log.info("Deleting class: {}", id);
+        Classes classes = classesRepository.findById(id).orElseThrow(() -> new RuntimeException("Class not found"));
+
+        classes.setDeletedAt(LocalDateTime.now());
     }
 
 
