8127ea8e2121c30eefe3da3ba165b50e1c102fb0
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-01 00:34:24 +0700

    feat/ add Material Request and Response DTO

---
+++ src/main/java/com/qiraht/spring_lms/dto/MaterialRequestDTO.java
@@ -0,0 +1,18 @@
+package com.qiraht.spring_lms.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MaterialRequestDTO {
+    @NotBlank
+    String title;
+    @NotBlank
+    String content;
+    String attachment;
+}
