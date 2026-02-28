edeb9ad85d715f835f1ee00d9467cff7bf6ea783
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-02-28 21:59:44 +0700

    feat/ add Material Entity

---
+++ src/main/java/com/qiraht/spring_lms/entity/Material.java
@@ -0,0 +1,45 @@
+package com.qiraht.spring_lms.entity;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "materials")
+public class Material {
+    @Id
+    private String id;
+
+    private String title;
+
+    private String content;
+
+    @Column(nullable = false)
+    private String attachment;
+
+    @Column(name = "class_id")
+    private String classId;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
