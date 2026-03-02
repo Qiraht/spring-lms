92e8b2957e83d3563be558cc77422aee165e7deb
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-02 16:16:52 +0700

    feat/ create UserRepository

---
+++ src/main/java/com/qiraht/spring_lms/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.qiraht.spring_lms.repository;
+
+import com.qiraht.spring_lms.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+
+
+}
