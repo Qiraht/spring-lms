8393295a20789fed2570e9712ccf734c953e099f
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-03 14:30:08 +0700

    chore/ fix status code return and add validation

--- src/main/java/com/qiraht/spring_lms/controller/ClassesController.java
+++ src/main/java/com/qiraht/spring_lms/controller/ClassesController.java
@@ -8,6 +8,7 @@ import com.qiraht.spring_lms.service.ClassesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/class")
 @RequiredArgsConstructor
+@Validated
 public class ClassesController {
     private final ClassesService classesService;
 
@@ -22,28 +24,28 @@ public class ClassesController {
     public ResponseEntity<ApiResponse<Void>> postClass(@RequestBody ClassRequestDTO request) {
         classesService.createClass(request);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(201,"Class created successfully", null));
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(HttpStatus.CREATED.value(),"Class created successfully", null));
     }
 
     @GetMapping
     public ResponseEntity<ApiResponse<List<ClassResponseDTO>>> getAllClasses() {
         List<ClassResponseDTO> data = classesService.getAllClasses();
 
-        return ResponseEntity.ok(ApiResponse.success(200, "success", data));
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(HttpStatus.OK.value(), "success", data));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse<ClassResponseDTO>> getClassById(@PathVariable("id") String id) {
         ClassResponseDTO data = classesService.getClassById(id);
 
-        return ResponseEntity.ok(ApiResponse.success(201,"success", data));
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(HttpStatus.OK.value(), "success", data));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ApiResponse<Classes>> putClass(@PathVariable("id") String id, @RequestBody ClassRequestDTO request) {
         classesService.updateClass(id, request);
 
-        return ResponseEntity.ok(ApiResponse.success(200,"success", null));
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(HttpStatus.OK.value(), "success", null));
     }
 
     @DeleteMapping("/{id}")
