348a7775a5442b4996c569639733972e086b0eb4
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-08 02:54:31 +0700

    feat: implement Resource Authorization on AssignmentController

--- src/main/java/com/qiraht/spring_lms/controller/AssignmentController.java
+++ src/main/java/com/qiraht/spring_lms/controller/AssignmentController.java
@@ -7,6 +7,8 @@ import com.qiraht.spring_lms.service.AssignmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,17 +16,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/assignment")
 @RequiredArgsConstructor
+@Validated
 public class AssignmentController {
     private final AssignmentService assignmentService;
 
     @PostMapping("/class/{classId}")
-    public ResponseEntity<ApiResponse<String>> postAssignment(@PathVariable String classId, @RequestBody AssignmentRequestDTO request) {
+    @PreAuthorize("hasRole('ADMIN') or @enrollmentService.isTeacherOfClass(authentication.principal.userId, #classId)")
+    public ResponseEntity<ApiResponse<String>> postAssignment(@PathVariable String classId,
+            @RequestBody AssignmentRequestDTO request) {
         String data = assignmentService.addAssignment(classId, request);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(HttpStatus.OK.value(), "Assignment on class " + classId +" created successfully", data));
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(HttpStatus.OK.value(),
+                "Assignment on class " + classId + " created successfully", data));
     }
 
     @GetMapping("/{assignmentId}")
+    @PreAuthorize("hasRole('ADMIN') or @enrollmentService.isEnrolledInClass(authentication.principal.userId, @assignmentRepository.findById(#assignmentId).get().classes.id)")
     public ResponseEntity<ApiResponse<AssignmentResponseDTO>> getAssignmentById(@PathVariable String assignmentId) {
         AssignmentResponseDTO data = assignmentService.getAssignmentById(assignmentId);
 
@@ -32,20 +39,25 @@ public class AssignmentController {
     }
 
     @GetMapping("/class/{classId}")
-    public ResponseEntity<ApiResponse<List<AssignmentResponseDTO>>> getAssignmentByClassId(@PathVariable String classId) {
+    @PreAuthorize("hasRole('ADMIN') or @enrollmentService.isEnrolledInClass(authentication.principal.userId, #classId)")
+    public ResponseEntity<ApiResponse<List<AssignmentResponseDTO>>> getAssignmentByClassId(
+            @PathVariable String classId) {
         List<AssignmentResponseDTO> data = assignmentService.getAssignmentsByClass(classId);
 
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(HttpStatus.OK.value(), "success", data));
     }
 
     @PutMapping("/{assignmentId}")
-    public ResponseEntity<ApiResponse<String>> putAssignment(@PathVariable String assignmentId, @RequestBody AssignmentRequestDTO request) {
+    @PreAuthorize("hasRole('ADMIN') or @authService.isTeacherOfAssignment(authentication.principal.userId, #assignmentId)")
+    public ResponseEntity<ApiResponse<String>> putAssignment(@PathVariable String assignmentId,
+            @RequestBody AssignmentRequestDTO request) {
         String data = assignmentService.editAssignment(assignmentId, request);
 
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(HttpStatus.OK.value(), "success", data));
     }
 
     @DeleteMapping("/{assignmentId}")
+    @PreAuthorize("hasRole('ADMIN')") // Admin Only
     public ResponseEntity<ApiResponse<String>> deleteAssignment(@PathVariable String assignmentId) {
         String data = assignmentService.deleteAssignment(assignmentId);
 
