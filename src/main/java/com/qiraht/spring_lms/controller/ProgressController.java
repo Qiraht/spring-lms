73e2a0a2dca9986377afcd461243eafb44bf4e9e
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-08 03:11:29 +0700

    feat: create student progress controller

---
+++ src/main/java/com/qiraht/spring_lms/controller/ProgressController.java
@@ -0,0 +1,40 @@
+package com.qiraht.spring_lms.controller;
+
+import com.qiraht.spring_lms.dto.ApiResponse;
+import com.qiraht.spring_lms.dto.response.StudentClassSummaryDTO;
+import com.qiraht.spring_lms.service.ProgressService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/class/{classId}")
+@RequiredArgsConstructor
+public class ProgressController {
+
+    private final ProgressService progressService;
+
+    @GetMapping("/summary")
+    @PreAuthorize("hasRole('ADMIN') or @enrollmentService.isTeacherOfClass(authentication.principal.userId, #classId)")
+    public ResponseEntity<ApiResponse<List<StudentClassSummaryDTO>>> getAllStudentSummaries(
+            @PathVariable String classId) {
+
+        List<StudentClassSummaryDTO> data = progressService.getAllStudentSummariesForClass(classId);
+        return ResponseEntity.ok(ApiResponse.success(HttpStatus.OK.value(), "success", data));
+    }
+
+    @GetMapping("/summary/{studentId}")
+    @PreAuthorize("hasRole('ADMIN') or @enrollmentService.isTeacherOfClass(authentication.principal.userId, #classId) or authentication.principal.userId == #studentId")
+    public ResponseEntity<ApiResponse<StudentClassSummaryDTO>> getStudentSummary(
+            @PathVariable String classId,
+            @PathVariable UUID studentId) {
+
+        StudentClassSummaryDTO data = progressService.getStudentClassSummary(classId, studentId);
+        return ResponseEntity.ok(ApiResponse.success(HttpStatus.OK.value(), "success", data));
+    }
+}
