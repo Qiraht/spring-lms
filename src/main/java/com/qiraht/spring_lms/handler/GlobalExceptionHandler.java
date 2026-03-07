ab6b1449853488dee0f12aa271523080d7ef56e1
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-08 00:11:30 +0700

    feat: add Authentication and Authorization Exception Handling and fix Not Found Exception handling

--- src/main/java/com/qiraht/spring_lms/handler/GlobalExceptionHandler.java
+++ src/main/java/com/qiraht/spring_lms/handler/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.qiraht.spring_lms.handler;
 
 import com.qiraht.spring_lms.dto.ApiResponse;
+import com.qiraht.spring_lms.exception.AuthenticationException;
+import com.qiraht.spring_lms.exception.AuthorizationException;
+import com.qiraht.spring_lms.exception.NotFoundException;
 import com.qiraht.spring_lms.exception.ValidationException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -23,12 +26,30 @@ public class GlobalExceptionHandler {
     }
 
     // Not Found Error
-    @ExceptionHandler(NoClassDefFoundError.class)
-    public ResponseEntity<ApiResponse<?>> handleBusinessError(NoClassDefFoundError ex, HttpServletRequest request) {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ApiResponse<?>> handleBusinessError(NotFoundException ex, HttpServletRequest request) {
         log.error("error while processing request", ex);
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(ApiResponse.error(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), null));
+                .body(ApiResponse.error(HttpStatus.NOT_FOUND.value(), ex.getMessage(), null));
+    }
+
+    // Authentication Error
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiResponse<?>> handleBusinessError(AuthenticationException ex, HttpServletRequest request) {
+        log.error("error while processing request", ex);
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(ApiResponse.error(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), null));
+    }
+
+    // Authorization Error
+    @ExceptionHandler(AuthorizationException.class)
+    public ResponseEntity<ApiResponse<?>> handleBusinessError(AuthorizationException ex, HttpServletRequest request) {
+        log.error("error while processing request", ex);
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(ApiResponse.error(HttpStatus.FORBIDDEN.value(), ex.getMessage(), null));
     }
 
     // Jakarta Validation Error
