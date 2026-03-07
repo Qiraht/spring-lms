8f9ef7d704397a65c612f7b194ec6b2db3999550
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-07 12:49:50 +0700

    chore/ change endpoint for UserController and move login to AuthController

--- src/main/java/com/qiraht/spring_lms/controller/UserController.java
+++ src/main/java/com/qiraht/spring_lms/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.qiraht.spring_lms.controller;
 
 import com.qiraht.spring_lms.dto.ApiResponse;
-import com.qiraht.spring_lms.dto.LoginRequestDTO;
 import com.qiraht.spring_lms.dto.RegisterRequestDTO;
 import com.qiraht.spring_lms.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -16,19 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 @Validated
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/register")
+    @PostMapping
     public ResponseEntity<ApiResponse<UUID>> postRegister(@RequestBody RegisterRequestDTO request) {
         UUID data = userService.RegisterUser(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(201,"success", data));
     }
-
-    @PostMapping("/login")
-    public void postLogin(@RequestBody LoginRequestDTO request) {}
 }
