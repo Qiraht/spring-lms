4b9367bc0b1ec3d492f137b6761977917e7908ec
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-08 03:08:06 +0700

    feat: add submission grade request dto

---
+++ src/main/java/com/qiraht/spring_lms/dto/request/GradeRequestDTO.java
@@ -0,0 +1,16 @@
+package com.qiraht.spring_lms.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GradeRequestDTO {
+    private BigDecimal score;
+}
