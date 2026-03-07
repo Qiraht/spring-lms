e7f55a60914fde3373444c4c5a35d51dfa792eb1
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-07 12:47:37 +0700

    feat/ add LoginResponseDTO

---
+++ src/main/java/com/qiraht/spring_lms/dto/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package com.qiraht.spring_lms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponseDTO {
+    private String email;
+    private String token;
+}
