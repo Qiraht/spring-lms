96d225b4455d6de3ffc5523eba2cb0f7906f2811
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-08 00:11:06 +0700

    feat: add Authentication and Authorization Exception

---
+++ src/main/java/com/qiraht/spring_lms/exception/AuthorizationException.java
@@ -0,0 +1,7 @@
+package com.qiraht.spring_lms.exception;
+
+public class AuthorizationException extends BusinessException {
+    public AuthorizationException(String message) {
+        super(message);
+    }
+}
