1f0de228381feec9ba8fd91ea8c598b8de8494cd
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-03-08 08:46:53 +0700

    feat: implement softDelete

--- src/main/java/com/qiraht/spring_lms/entity/Material.java
+++ src/main/java/com/qiraht/spring_lms/entity/Material.java
@@ -3,6 +3,7 @@ package com.qiraht.spring_lms.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "materials")
+@SQLRestriction("deleted_at IS NULL")
 public class Material {
     @Id
     private String id;
