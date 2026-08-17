5c2da8c2ec8cd0cf5ace3dccb2a2308f3ac1d2e2
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-08-17 12:39:13 +0700

    fix(jpa): disable OSIV and eager-load associations

--- src/main/java/com/qiraht/spring_lms/repository/EnrollmentRepository.java
+++ src/main/java/com/qiraht/spring_lms/repository/EnrollmentRepository.java
@@ -5,6 +5,7 @@ import com.qiraht.spring_lms.entity.Enrollment;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, UUID> {
@@ -14,5 +15,6 @@ public interface EnrollmentRepository extends JpaRepository<Enrollment, UUID> {
 
     java.util.Optional<Enrollment> findByClassesIdAndUserId(UUID classId, UUID userId);
 
+    @EntityGraph(attributePaths = "user")
     Page<Enrollment> findByClassesIdAndRole(UUID classId, ClassRole role, Pageable pageable);
 }
